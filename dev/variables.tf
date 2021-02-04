ab5ac578799d10785aaa9e903e4bf369f5bd076f
ShajithSritharan <shajithshri@gmail.com>
2021-02-04 18:40:40 -0500

    Added variable

---
+++ dev/variables.tf
@@ -0,0 +1,22 @@
+variable "filename" {
+  default = "/home/ubuntu/pet.txt"
+}
+
+variable "content" {
+  default = "We love pets!"
+
+}
+
+variable "prefix" {
+  default = "Mrs"
+}
+
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = "1"
+}
+
+
